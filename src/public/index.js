1d239abda7860b05356ec81b30a5811c59683acd
Thomas Salah <thomas.salah@gmail.com>
2021-05-30 16:09:35 +0100

    chore: adding a few packages + dummy index.js to test linter ect

--- src/public/index.js
+++ src/public/index.js
@@ -1,1 +1,11 @@
 // Hello world
+const init = () => {
+  const body = document.getElementById('root')
+  const title = document.createElement('h1')
+
+  title.innerHTML = 'yo'
+  body.appendChild(title)
+  return body
+}
+
+init()
